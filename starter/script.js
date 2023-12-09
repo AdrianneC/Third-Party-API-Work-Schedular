1a8a3210eaf43bedf47d15ba88fb33198791d1cd
Adrianne <96875139+AdrianneC@users.noreply.github.com>
2023-12-09 00:25:26 +0000

    Included further JS code

--- starter/script.js
+++ starter/script.js
@@ -7,3 +7,34 @@ function showDay() {
     $("#currentDay").text(currentDay);
 }
 showDay();
+
+
+// JS to add project to the hour time block
+
+
+// $(".task-button").on('click', function() {
+// var task = $("<textarea>");
+// task.addClass("description");
+//  task.text($(this).attr("textarea"));
+//   $("#textarea").append(task);
+//      });
+
+
+
+
+// JS to highlight colour
+
+var currentTime = dayjs().format('HH:mm')
+var hoursOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+console.log(currentTime)
+
+
+// for (var i = 0; i < timeBlock.length; i++) {
+
+//     if hourNum <= currentTime
+//     letterBtn.text(letters[i]);
+//     $("#buttons").append(letterBtn);
+
+//   }
+
+// if hour =
